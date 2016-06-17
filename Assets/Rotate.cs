52b43db6d46ae967220df6a5e46be269490867c3
Oscar Losada <oscarlosu@gmail.com>
2016-06-17 10:05:24 +0200

    Changed default color of triangles to white.

---
+++ Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    public float angle = 90;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, angle * Time.deltaTime);
+	}
+}
